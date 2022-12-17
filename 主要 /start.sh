4d3baf22524f50af73b4312f384e2a5e1d19da2c
werfgh56j <91049940+werfgh56j@users.noreply.github.com>
2022-12-17 13:03:22 +0800

    Update and rename main/start.sh to 主要 /start.sh

--- 主要 /start.sh
+++ 主要 /start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-607589a6e0b14822b088c3b4cd42e8fa}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
